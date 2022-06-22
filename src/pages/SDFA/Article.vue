4773bf8f6a81280d691f63ea4a47b216d5b1073a
yuki-chai <chaiyujin@gmail.com>
2022-06-22 13:57:30 +0800

    Update Article.vue

--- src/pages/SDFA/Article.vue
+++ src/pages/SDFA/Article.vue
@@ -58,10 +58,26 @@
                 <svg :height="36 * page_scale" viewBox="0 0 16 16" version="1.1" width="32" aria-hidden="true"><path fill-rule="evenodd" d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.013 8.013 0 0016 8c0-4.42-3.58-8-8-8z"></path></svg>
               </v-list-item-action>
               <v-list-item-content  class='pending'>
-                GitHub Repository <br/> with Pre-trained Model (Available Soon)
+                <a href="https://github.com/chaiyujin/sdfa-2019" target="_blank" >GitHub Repository</a>
               </v-list-item-content>
+              <!-- <v-list-item-content>
+                https://github.com/chaiyujin/sdfa-2019
+              </v-list-item-content> -->
             </v-list-item>
           </v-list>
+          <h2> Citation </h2>
+          <p class="code">
+          @article{chai2022speech,<br/>
+          {{'\xa0\xa0'}}title={Speech-driven facial animation with spectral gathering and temporal attention},<br/>
+          {{'\xa0\xa0'}}author={Chai, Yujin and Weng, Yanlin and Wang, Lvdi and Zhou, Kun},<br/>
+          {{'\xa0\xa0'}}journal={Frontiers of Computer Science},<br/>
+          {{'\xa0\xa0'}}volume={16},<br/>
+          {{'\xa0\xa0'}}number={3},<br/>
+          {{'\xa0\xa0'}}pages={1--10},<br/>
+          {{'\xa0\xa0'}}year={2022},<br/>
+          {{'\xa0\xa0'}}publisher={Springer}<br/>
+          }
+          </p>
         </div>
 
       </div>
@@ -118,5 +134,11 @@ export default {
 }
 .pending {
   color: gray;
+}
+.code {
+  background-color: rgb(235, 235, 235);
+  padding: 10px;
+  font-family: 'Courier New', Courier, monospace;
+  font-size: 0.8em;
 }
 </style>
